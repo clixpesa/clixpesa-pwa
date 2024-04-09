677cccd60d0eb3a429aa6decf484305cddcb0983
Kachisa D.N <kachdekan@gmail.com>
2024-04-09 19:46:36 +0300

    Update firebase.config.js

--- packages/dapp/firebase.config.js
+++ packages/dapp/firebase.config.js
@@ -10,13 +10,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAU4hXLXOSRh2ZXK4OUYYfvY7qH2MCBAcA',
-  authDomain: 'clixpesa-48d7e.firebaseapp.com',
-  projectId: 'clixpesa-48d7e',
-  storageBucket: 'clixpesa-48d7e.appspot.com',
-  messagingSenderId: '122542045439',
-  appId: '1:122542045439:web:f56ca3373494111d7cb4ca',
-  measurementId: 'G-V46LT1K4XV',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 // Initialize Firebase
